f8f920ff10d6aa14a4c6249f3fa9a82736d84ae5
nichanank <n.kesonpat@gmail.com>
2020-04-28 17:24:18 +0700

    remove server directory from main project repo

--- packages/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-/* GET all listing. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'PeerStream'})
-})
-
-module.exports = router
